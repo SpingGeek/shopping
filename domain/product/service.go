3d4f9f993c4f901ee9f93c628428219631e62b3c
SpringGeek <springoflotus@gmail.com>
2025-01-31 11:33:24 +0800

    optimize ：添加相关注释

--- domain/product/service.go
+++ domain/product/service.go
@@ -7,10 +7,12 @@ import (
 	"shopping/utils/pagination"
 )
 
+// 商品服务结构体
 type Service struct {
 	productRepository Repository
 }
 
+// Service 实例化
 func NewService(productRepository Repository) *Service {
 	productRepository.Migration()
 	return &Service{
@@ -18,6 +20,7 @@ func NewService(productRepository Repository) *Service {
 	}
 }
 
+// 获得所有商品分页
 func (c *Service) GetAll(page *pagination.Pages) *pagination.Pages {
 	products, count := c.productRepository.GetAll(page.Page, page.PageSize)
 	page.Items = products
@@ -25,22 +28,26 @@ func (c *Service) GetAll(page *pagination.Pages) *pagination.Pages {
 	return page
 }
 
+// 创建商品
 func (c *Service) CreateProduct(name string, decs string, count int, price float32, cid uint) error {
 	newProduct := NewProduct(name, decs, count, price, cid)
 	err := c.productRepository.Create(newProduct)
 	return err
 }
 
+// 删除商品
 func (c *Service) DeleteProduct(sku string) error {
 	err := c.productRepository.Delete(sku)
 	return err
 }
 
+// 更新商品
 func (c *Service) UpdateProduct(product *Product) error {
 	err := c.productRepository.Update(*product)
 	return err
 }
 
+// 查找商品
 func (c *Service) SearchProduct(text string, page *pagination.Pages) *pagination.Pages {
 	products, count := c.productRepository.SearchByString(text, page.Page, page.PageSize)
 	page.Items = products
