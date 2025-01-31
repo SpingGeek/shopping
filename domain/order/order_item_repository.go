9b1b6464cbefdd4ce8c592b5ae0a4655bc32c792
SpringGeek <springoflotus@gmail.com>
2025-01-31 16:33:41 +0800

    feature : 订单项数据库操作

---
+++ domain/order/order_item_repository.go
@@ -0,0 +1,53 @@
+// @Author Gopher
+// @Date 2025/01/31 16:27:58
+// @Desc
+package order
+
+import (
+	"log"
+
+	"gorm.io/gorm"
+)
+
+type OrderedItemRepository struct {
+	db *gorm.DB
+}
+
+// 实例化
+func NewOrderedItemRepository(db *gorm.DB) *OrderedItemRepository {
+	return &OrderedItemRepository{
+		db: db,
+	}
+}
+
+// 创建表
+func (r *OrderedItemRepository) Migration() {
+	err := r.db.AutoMigrate(&OrderedItem{})
+	if err != nil {
+		log.Print(err)
+	}
+}
+
+// 更新
+func (r *OrderedItemRepository) Update(item OrderedItem) error {
+	result := r.db.Save(&item)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+// 创建订单item
+func (r *OrderedItemRepository) Create(ci *OrderedItem) error {
+	result := r.db.Create(ci)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	return nil
+}
+
+
